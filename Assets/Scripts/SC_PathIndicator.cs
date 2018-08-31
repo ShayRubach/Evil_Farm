a2b5ce2bda6b2a25ac097f62f579bfba3d57d84c
ShayRubach <shayrubach@gmail.com>
2018-09-01 01:37:45 +0200

    refactored PathIndator script

--- Assets/Scripts/SC_PathIndicator.cs
+++ Assets/Scripts/SC_PathIndicator.cs
@@ -6,50 +6,38 @@ using UnityEngine;
 public class SC_PathIndicator : MonoBehaviour {
     private Ray ray;
     private RaycastHit hit;
-    private static float spinSpeed = 10.0f;
-    private bool spin = false;
-    private GameObject leafIndicatorsParent = null;
+    private static float spinSpeed = 15.0f;
+    private bool shouldSpin = false;
     private GameObject leafObj = null;
-    private Animator leaf_path_indicator_animator;
     private Quaternion originalRotation;
 
     void Start() {
-        leafIndicatorsParent = GameObject.Find("path_indicators");
         originalRotation = transform.rotation;
     }
 
     void FixedUpdate() {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit)) {
-            string objHitName = hit.collider.name;
 
-            if (objHitName == gameObject.name) {
-                Debug.Log("hit " + gameObject.name);
-                leafObj = hit.collider.gameObject;
-                spin = true;
-            }
-            else {
-                spin = false;
-            }
-
-            if (spin) {
-                SpinLeaf(leafObj);
-            }
-            else {
-                ResetLeafRotation(leafObj);
-            }
+            shouldSpin = HoveredOverIndicator(hit.collider.name);
+        
+            if (shouldSpin) 
+                SpinLeaf();
+            else
+                ResetLeafRotation();
         }
     }
 
-    private void ResetLeafRotation(GameObject leaf) {
-        if (leaf) {
-            transform.rotation = originalRotation;
-        }
+    private bool HoveredOverIndicator(string objHitName) {
+        return (objHitName == gameObject.name);
     }
 
-    private void SpinLeaf(GameObject leaf) {
-        if(leaf)
-            transform.Rotate(Vector3.forward * spinSpeed);
+    private void ResetLeafRotation() {
+        transform.rotation = originalRotation;
+    }
+
+    private void SpinLeaf() {
+        transform.Rotate(Vector3.forward * spinSpeed);
     }
 
 }
